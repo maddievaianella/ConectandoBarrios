472a8bee6afa792e74f0fb3a6fd2f72a6377b028
maddievaianella <vaianellamadeleine@gmail.com>
2024-09-02 10:45:15 -0300

    Add JS

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,12 @@
-alert("prueba");
+// Selecciona el botón y el formulario
+const form = document.getElementById('contactForm');
+const submitBtn = document.getElementById('submitBtn');
+
+// 
+submitBtn.addEventListener('click', function() {
+    // "Envía" 
+    form.reset();
+    
+    // Mensaje Alert
+    alert('Gracias por enviarnos tu mensaje');
+});
